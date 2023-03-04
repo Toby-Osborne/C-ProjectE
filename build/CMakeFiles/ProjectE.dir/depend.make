bff05480a8d512cfd6a5322889664143c9e690fe
Toby Osborne <tosb789@aucklanduni.ac.nz>
2023-03-05 00:51:05 +1300

    git: added gitignore and removed build directory

--- build/CMakeFiles/ProjectE.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for ProjectE.
-# This may be replaced when dependencies are built.
--- build/CMakeFiles/ProjectE.dir/projecte.c.o.d
+++
@@ -1,5 +0,0 @@
-CMakeFiles/ProjectE.dir/projecte.c.o: \
- /home/tazukiswift/Programming/C-Project-Euler/projecte.c \
- /usr/include/stdc-predef.h \
- /home/tazukiswift/Programming/C-Project-Euler/build/ProjectEConfig.h \
- /home/tazukiswift/Programming/C-Project-Euler/./questions/questions.h
